45bf9b82821d3a4bae6143ae92ec2c9407c3b0bb
Marco André Mendes <marcoandre@gmail.com>
2024-03-18 11:04:35 -0300

    Formatação com isort e black

--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -2,7 +2,8 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Editora
 
+
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
-        fields = '__all__'
+        fields = "__all__"
--- core/views/editora.py
+++ core/views/editora.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Editora
 from core.serializers import EditoraSerializer
 
+
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
-    
