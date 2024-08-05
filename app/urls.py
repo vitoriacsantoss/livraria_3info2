7eaf3466d178cca5007e8c0ab8d60cfc13b1023b
Marco André Mendes <marcoandre@gmail.com>
2024-08-05 11:58:38 -0300

    Inicio da configuração de multiplos serializadores para Livro.

--- app/urls.py
+++ app/urls.py
@@ -9,12 +9,13 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet, basename="autores")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"editoras", EditoraViewSet, basename="editoras")
+router.register(r"livros", LivroViewSet, basename="livros")
 router.register(r"usuarios", UserViewSet, basename="users")
 
 urlpatterns = [
