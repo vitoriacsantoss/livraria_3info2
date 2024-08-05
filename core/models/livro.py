52e6871d8dae0d89721331da9c67b64c2be9d2dc
Marco André Mendes <marcoandre@gmail.com>
2024-08-05 10:49:21 -0300

    Adicioados relacionamentos para autor e coautor.

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+from .categoria import Categoria
+from .editora import Editora
+from .autor import Autor
+
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, blank=True, null=True)
     quantidade = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", blank=True, null=True)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros_autor", blank=True, null=True)
+    coautor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name="livros_coautor", blank=True, null=True)
+
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
